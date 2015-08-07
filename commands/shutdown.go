4fed047e89c1efa7d550cc17ba3539e2a5a56aae
Michael Boudreau <mkboudreau@us.ibm.com>
2015-08-07 14:58:03 -0700

    Add hooks to handle os signals for shutting down

---
+++ commands/shutdown.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func osSignalShutdown(doBeforeShutdown func(), shutdownDelay int) {
+	sigc := make(chan os.Signal, 2)
+
+	signal.Notify(sigc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	go func(s <-chan os.Signal, fn func(), delay int) {
+		str := <-s
+		log.Println("Received signal:", str.String())
+		fn()
+
+		log.Printf("Shutting down after %v seconds\n", delay)
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		os.Exit(0)
+	}(sigc, doBeforeShutdown, shutdownDelay)
+}
